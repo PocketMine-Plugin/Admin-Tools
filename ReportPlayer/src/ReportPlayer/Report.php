e30b965bcf213602da01d8397db67d5116b7a5ff
IceCruelStuff <50642756+IceCruelStuff@users.noreply.github.com>
2020-07-26 20:11:02 -0700

    Update Report.php

--- ReportPlayer/src/ReportPlayer/Report.php
+++ ReportPlayer/src/ReportPlayer/Report.php
@@ -1,32 +1,34 @@
 <?php
 
-/*
-__PocketMine Plugin__
-name=Report
-version=1.0.2
-author=ZacHack
-class=Report
-apiversion=10
-*/
+namespace ReportPlayer;
 
-class Report implements Plugin{
-	private $api, $message, $player;
-	public function __construct(ServerAPI $api, $server = false){
-		$this->api = $api;
-		$this->server = ServerAPI::request();
-	}
-	
-	public function init(){
-		$this->config = new Config($this->api->plugin->configPath($this)."Reports.yml", CONFIG_YAML, array(
+use pocketmine\command\Command;
+use pocketmine\command\CommandSender;
+use pocketmine\plugin\PluginBase;
+
+class Report extends PluginBase {
+
+	public $config;
+
+	private $api;
+	private $message;
+	private $player;
+
+	public function onEnable() : void {
+		$this->getServer()->getPluginManager()->registerEvents($this, $this);
+		if (!file_exists($this->getDataFolder() . "Reports.yml")) {
+			$this->saveResource("Reports.yml");
+		}
+
+		$this->config = new Config($this->getDataFolder() . 'Reports.yml', Config::YAML, array(
 			"Reports" => array(),
 		));
-		$this->api->console->register("report", "Report a player for the owner", array($this, "cmd"));
-		$this->config = $this->api->plugin->readYAML($this->api->plugin->configPath($this) ."Reports.yml");
-		$this->server->api->ban->cmdWhitelist("report");
+
+		$this->config->save();
 	}
-	
-	public function cmd($cmd, $args, $issuer, $params){
-		switch($cmd){
+
+	public function onCommand(CommandSender $sender, Command $command, string $label, array $args) : bool {
+		switch ($command->getName()) {
 			case "report":
 				$name = $issuer->username;
 				$msg = implode(" ", $args);
@@ -36,5 +38,9 @@ class Report implements Plugin{
 		}
 		return $output;
 	}
-	public function __destruct(){}
+
+	public function __destruct() {
+
+	}
+
 }
